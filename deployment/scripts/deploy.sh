9c90a3c69438451e5e08580af4c35b777b68f193
Christophe Bismuth <christophe.bismuth@gmail.com>
2016-09-09 12:30:09 +0200

    Add Maven debug option on deployment

--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -5,5 +5,5 @@ set -u
 set -e
 
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P sign,build-extras --settings $TRAVIS_BUILD_DIR/deployment/maven/settings.xml
+    mvn -X deploy -P sign,build-extras --settings $TRAVIS_BUILD_DIR/deployment/maven/settings.xml
 fi
