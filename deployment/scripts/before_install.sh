64b7b05158a9dcaf20dd6c88b44e053eb332cf54
Christophe Bismuth <christophe.bismuth@gmail.com>
2016-09-09 12:20:03 +0200

    Add debug options to deployment scripts

--- deployment/scripts/before_install.sh
+++ deployment/scripts/before_install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set +x
+set -u
+set -e
+
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_0b8b24b83425_key -iv $encrypted_0b8b24b83425_iv -in $TRAVIS_BUILD_DIR/deployment/crypto/codesigning.asc.enc -out $TRAVIS_BUILD_DIR/deployment/crypto/codesigning.asc -d
     gpg --fast-import $TRAVIS_BUILD_DIR/deployment/crypto/codesigning.asc
--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set +x
+set -u
+set -e
+
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy -P sign,build-extras --settings $TRAVIS_BUILD_DIR/deployment/maven/settings.xml
 fi
