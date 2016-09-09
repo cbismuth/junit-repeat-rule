65833c656c52b73985da025e1c8ef8ac684ac1e3
Christophe Bismuth <christophe.bismuth@gmail.com>
2016-09-09 13:01:47 +0200

    Figure out available Travis-CI environment variables

--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -3,6 +3,8 @@
 set +x
 set -e
 
+set
+
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn -X -Dgpg.passphrase=${GPG_PASSPHRASE} deploy -P sign,build-extras --settings $TRAVIS_BUILD_DIR/deployment/maven/settings.xml
 fi
