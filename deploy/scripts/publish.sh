f675d492e4c4a93d827f34336e1c5f970c229924
Christophe Bismuth <christophe.bismuth@gmail.com>
2016-09-10 14:55:56 +0200

    Add Coveralls Maven configuration

--- deploy/scripts/publish.sh
+++ deploy/scripts/publish.sh
@@ -5,9 +5,9 @@ set -e
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "Git tag detected - set Maven POM version to [$TRAVIS_TAG]"
-    mvn --settings $DEPLOY_DIR/maven/settings.xml versions:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    mvn versions:set -DnewVersion=$TRAVIS_TAG --settings $DEPLOY_DIR/maven/settings.xml
 else
     echo "No Git tag detected - keep Maven POM version untouched"
 fi
 
-mvn --settings $DEPLOY_DIR/maven/settings.xml -P ossrh clean deploy -B -U
+mvn clean deploy -P ossrh --settings $DEPLOY_DIR/maven/settings.xml -B -U
