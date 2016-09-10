ece0ab1614cadd5285b335c24b14db9e0087e3ed
Christophe Bismuth <christophe.bismuth@gmail.com>
2016-09-10 10:38:58 +0200

    Shorten Versions Maven Plugin usage

--- deploy/scripts/publish.sh
+++ deploy/scripts/publish.sh
@@ -6,7 +6,7 @@ set -e
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "Git tag detected - set Maven POM version to [$TRAVIS_TAG]"
-    mvn --settings $DEPLOY_DIR/maven/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    mvn --settings $DEPLOY_DIR/maven/settings.xml versions:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
 else
     echo "No Git tag detected - keep Maven POM version untouched"
 fi
